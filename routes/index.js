2400c7b29d59b2b9a5ef493aaf63be25aa50659a
Margarita Shumilina <margar.maksim@ya.ru>
2023-05-12 01:51:56 +0300

    ПР -14 (неполная версия-2)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const cardRouter = require('./cards');
 const userRouter = require('./users');
-const apiRouter = require('./api');
+// const apiRouter = require('./api');
 const auth = require('../middlewares/auth');
+const { login, createUser } = require('../controllers/api');
 
 router.use('/users', auth, userRouter);
 router.use('/cards', auth, cardRouter);
-router.use('/api', apiRouter);
+router.post('/signin', login);
+router.post('/signup', createUser);
 
 module.exports = router;
