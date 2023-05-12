518607096ba1732a739bbbfc5645c1cc08150ac7
Margarita Shumilina <margar.maksim@ya.ru>
2023-05-12 23:41:43 +0300

    ПР -14 (неполная версия-11)

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,40 @@
 const apiRouter = require('express').Router();
-const { errors, celebrate, Joi } = require("celebrate");
+const { errors, celebrate, Joi } = require('celebrate');
 const {
   login,
   createUser,
 } = require('../controllers/api');
 
-apiRouter.post('/signin', login);
-apiRouter.post('/signup', createUser);
+apiRouter.post(
+  '/signin',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        email: Joi.string()
+          .required()
+          .email({ tlds: { allow: false } }),
+        password: Joi.string().required(),
+      })
+      .unknown(false),
+  }),
+  login,
+);
+apiRouter.post(
+  '/signup',
+  celebrate({
+    body: Joi.object()
+      .keys({
+        email: Joi.string()
+          .required()
+          .email({ tlds: { allow: false } }),
+        password: Joi.string().required(),
+        about: Joi.string().min(2).max(30),
+        name: Joi.string().min(2).max(30),
+        avatar: Joi.string().custom(validateUrl, 'custom validate url'),
+      })
+      .unknown(false),
+  }),
+  createUser
+);
 
 module.exports = apiRouter;
