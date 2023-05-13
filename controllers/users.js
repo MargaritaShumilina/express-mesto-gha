f329a3df0b4b647d2b1a2b53153fc766117231a2
Margarita Shumilina <margar.maksim@ya.ru>
2023-05-13 12:36:32 +0300

    ПР -14 (версия 5)

--- controllers/users.js
+++ controllers/users.js
@@ -64,80 +64,9 @@ const updateUserData = (req, res, next) => {
       }
       next(err);
     });
-  // const owner = req.user._id;
-  // const { name, about } = req.body;
-
-  // User.findByIdAndUpdate(
-  //   owner,
-  //   { name, about },
-  //   { new: true, runValidators: true }
-  // )
-  //   .then((user) => userMe(user, res))
-  //   .catch(next);
-  // User.findByIdAndUpdate(req.user._id, req.body, {
-  //   new: true,
-  //   runValidators: true,
-  // })
-  //   .orFail(
-  //     () => new PAGE_NOT_FOUND('Пользователь с указанным id не существует')
-  //   )
-  //   .then((user) => {
-  //     res.status(200).send({ newObject: user });
-  //   })
-  //   .catch((err) => {
-  //     if (err.name === 'ValidationError') {
-  //       next(new BAD_REQUEST('Переданы некоректные данные'));
-  //     } else {
-  //       next(err);
-  //     }
-  //   });
-  // const userId = req.user._id;
-
-  // const { name, about } = req.body;
-
-  // User.findByIdAndUpdate(
-  //   userId,
-  //   {
-  //     name,
-  //     about,
-  //   },
-  //   {
-  //     new: true,
-  //     runValidators: true,
-  //   },
-  // )
-  //   .then((user) => {
-  //     userMe(user, res);
-  //   })
-  //   .catch((err) => {
-  //     if (err.name === 'ValidationError') {
-  //       next(new BAD_REQUEST('Переданы некорректные данные при обновлении профиля'));
-  //     }
-  //     next(err);
-  //   });
 };
 
 const updateUserAvatar = (req, res, next) => {
-  // const owner = req.user._id;
-  // const avatar = req.body;
-
-  // User.findByIdAndUpdate(owner, avatar, { new: true, runValidators: true })
-  //   .then((user) => userMe(user, res))
-  //   .catch(next);
-  // const { avatar } = req.body;
-
-  // User.findByIdAndUpdate(
-  //   req.user._id,
-  //   { avatar },
-  //   { new: true, runValidators: true }
-  // )
-  //   .orFail(
-  //     () => new PAGE_NOT_FOUND('Пользователь с указанным id не существует')
-  //   )
-  //   .then((sendAvatar) => {
-  //     res.status(200).send(sendAvatar);
-  //   })
-  //   .catch(next);
   const userId = req.user._id;
 
   const { avatar } = req.body;
@@ -167,36 +96,9 @@ const getMyProfile = (req, res, next) => {
       if (!user) {
         next(new PAGE_NOT_FOUND("Пользователь не найден"));
       }
-      res.send(user);
+      res.send({ data: user });
     })
     .catch((err) => next(err));
-  // const userId = req.user._id;
-
-  // User.findById(userId)
-  //   .orFail(() => new PAGE_NOT_FOUND("NotFound"))
-  //   .then((user) => res.send(user))
-  //   .catch((err) => {
-  //     if (err.message === "NotFound") {
-  //       next(new PAGE_NOT_FOUND("NotFound"));
-  //     }
-
-  //     if (err.name === "CastError") {
-  //       next(new BAD_REQUEST("Невалидный id "));
-  //     }
-
-  //     next(err);
-  //   });
-
-  // User.findById(userId)
-  //   .orFail(() => new PAGE_NOT_FOUND('Пользватель с указанным id не существует'))
-  //   .then((user) => res.send(user))
-  //   .catch((err) => {
-  //     if (err.name === 'CastError') {
-  //       next(new BAD_REQUEST('Невалидный id'));
-  //       return;
-  //     }
-  //     next(err);
-  //   });
 };
 
 module.exports = {
